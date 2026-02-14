883bcc359f308b36100c6cde879a3bfdf6135687
HarunaT-dev <wyolw239@gmail.com>
2026-02-14 17:17:23 +0900

    feat: complete while-loop health simulation with perfect variable consistency

---
+++ health_check.php
@@ -0,0 +1,19 @@
+<?php
+
+$energy = 100;
+$hour = 1;
+
+while ($energy >= 20) {
+
+    $energy -= 15;
+
+    echo $hour . "時間目: 残りエネルギー" . $energy . "<br>\n";
+
+    if ($energy < 20) {
+        echo "健康第一！休息が必要です<br>\n";
+
+    }
+
+    $hour++;
+}
+?>   
